625b7d5619f4079125e2c4210ac545dd9db0ce5f
xinhl <xinhl@microsoft.com>
2022-03-25 05:10:34 +0000

    skip location check if cname returns only privatelink

--- main.go
+++ main.go
@@ -58,7 +58,9 @@ func main() {
 	logger.V(2).Info("Canonical name for ACR (%s): %s", acr, cname)
 
 	acrLocation := strings.Split(cname, ".")[1]
-	logger.V(2).Info("ACR location: %s", acrLocation)
+	if !strings.EqualFold(acrLocation, "privatelink")  {
+		logger.V(2).Info("ACR location: %s", acrLocation)
+	}
 
 	azConfigPath := *configPath
 	if *configPath == "" {
@@ -83,7 +85,7 @@ func main() {
 		os.Exit(exitcode.AzureConfigUnmarshalFailure)
 	}
 
-	if !utils.LocationEquals(acrLocation, cfg.Location) && !strings.EqualFold(acrLocation, "chinaeast2")  {
+	if !utils.LocationEquals(acrLocation, cfg.Location) && !strings.EqualFold(acrLocation, "privatelink")  {
 		logger.V(2).Info("Checking ACR location matches cluster location: FAILED")
 		logger.V(2).Info("ACR location '%s' does not match your cluster location '%s'. This may result in slow image pulls and extra cost.", acrLocation, cfg.Location)
 	}
